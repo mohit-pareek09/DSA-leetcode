ac1c35829980677c268c43acac680935f6123256
Mohit Pareek <prkmohit14@gmail.com>
2025-07-30 11:33:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Head of Linked List/delete-head-of-linked-list.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+    }
+
+};
+
+*/
+// Complete this function
+Node *deleteHead(Node *head) {
+    // Your code here
+    Node* temp=head;
+    head=head->next;
+    free(temp);
+    
+    return head;
+}
