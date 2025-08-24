46c4e9726904e274804538063a7ad85d0de5ca34
Mohit Pareek <prkmohit14@gmail.com>
2025-08-24 19:06:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Division without using multiplication, division and mod operator/division-without-using-multiplication-division-and-mod-operator.cpp
@@ -0,0 +1,9 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    long long divide(long long dividend, long long divisor) {
+        // code here
+        return dividend/divisor;
+    }
+};
