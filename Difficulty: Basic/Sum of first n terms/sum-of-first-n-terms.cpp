898d2b97ff3f7ead7c2bc18afb47ba5fda1b7972
Mohit Pareek <prkmohit14@gmail.com>
2025-10-29 17:10:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of first n terms/sum-of-first-n-terms.cpp
@@ -0,0 +1,11 @@
+// User function template for C++
+
+class Solution {
+  public:
+    int sumOfSeries(int n) {
+        // code her
+        int ans = ((n*n)*((n+1)*(n+1)))/4;
+        return ans;
+        
+    }
+};
